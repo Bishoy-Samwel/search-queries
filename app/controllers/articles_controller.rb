afca8da59a376dcfee545233b15f859bad438b26
Bishoy Samwel <bishoy.samwuel@gmail.com>
2024-01-13 18:39:59 +0200

    fix: set ip_address for index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
-  before_action :set_user_ip_address, only: %i[ show edit update destroy ]
+  before_action :set_user_ip_address, only: %i[ index show edit update destroy ]
 
   # GET /articles or /articles.json
   def index
