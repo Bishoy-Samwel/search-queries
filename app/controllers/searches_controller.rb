fa0dcc78c52b723d0a26bd827761b08053af856d
Bishoy Samwel <bishoy.samwuel@gmail.com>
2024-01-13 15:13:58 +0200

    feat: display searches

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,7 +4,6 @@ class SearchesController < ApplicationController
 
   def create
     query_value = params.dig(:query)
-
     larger_queries = Search.larger_queries(query_value)
     if larger_queries.empty?
       Search.create(query: query_value)
@@ -13,6 +12,17 @@ class SearchesController < ApplicationController
         record.destroy if record.query.length < query_value.length
       end
     end
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.update("searches",
+                              partial: "searches/searches_list",
+                              locals: { searches: Search.all })
+        ]
+      end
+    end
+
   end
 
   def update
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,5 +1,5 @@
 class Search < ApplicationRecord
-  scope :larger_queries, -> (query_value) { find_by_sql(["SELECT * FROM searches WHERE query ILIKE ? || '%'", query_value])
+  scope :larger_queries, -> (query_value) { find_by_sql(["SELECT * FROM searches WHERE query ILIKE '%' || ? || '%'", query_value])
   }
-  scope :smaller_queries, -> (query_value) { find_by_sql(["SELECT * FROM searches WHERE ? ILIKE query || '%'", query_value]) }
+  scope :smaller_queries, -> (query_value) { find_by_sql(["SELECT * FROM searches WHERE ? ILIKE '%' || query || '%'", query_value]) }
 end
